a3fe4989b5313e9b998214a647d63408a0a15482
anike <anikeevleonid@gmail.com>
2025-04-09 21:20:48 +0300

    Merge remote-tracking branch 'origin/main'

--- apps/db_train/views.py
+++ apps/db_train/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.views import View
+from .models import Author, AuthorProfile, Entry, Tag
+
 # from .models import ...
 
 
 class TrainView(View):
     def get(self, request):
-        context = {}  # Создайте здесь запросы к БД
+
+
+
+        context = {f'answer{index}': self.__dict__[f'answer{index}'] for index in range(1, 11)}
+
         return render(request, 'train_db/training_db.html', context=context)
 
