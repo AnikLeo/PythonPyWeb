f166f3ece0c9f07651bcf2597dff219b997d563b
anike <anikeevleonid@gmail.com>
2025-05-04 17:11:00 +0300

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	apps/api/views.py

--- apps/api/views.py
+++ apps/api/views.py
@@ -14,6 +14,8 @@ from rest_framework.pagination import PageNumberPagination
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
 from rest_framework import permissions
+from rest_framework import authentication
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 
 class AuthorAPIView(APIView):
@@ -104,7 +106,9 @@ class AuthorGenericAPIView(GenericAPIView, RetrieveModelMixin, ListModelMixin, C
     queryset = Author.objects.all()
     serializer_class = AuthorModelSerializer
 
-    permission_classes = [CustomPermission]
+    permission_classes = [permissions.IsAuthenticated]
+    # authentication_classes = [authentication.TokenAuthentication]
+    authentication_classes = [JWTAuthentication]
 
     def get(self, request, *args, **kwargs):
         if kwargs.get(self.lookup_field):  # если был передан id или pk
